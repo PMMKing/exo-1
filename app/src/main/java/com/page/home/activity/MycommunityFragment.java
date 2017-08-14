73ccd0ad006cc3e65cc81f840fd4d035a33a4e55
shucheng.qu <shucheng.qu@jiulvxing.com>
2017-08-14 16:22:35 +0800

    qushucheng

--- app/src/main/java/com/page/home/activity/MycommunityFragment.java
+++ app/src/main/java/com/page/home/activity/MycommunityFragment.java
@@ -2,6 +2,7 @@ package com.page.home.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,19 @@ import android.view.ViewGroup;
 import com.haolb.client.R;
 import com.haolb.client.activity.BaseFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by chenxi.cui on 2017/8/13.
  */
 
 public class MycommunityFragment extends BaseFragment {
+
+    @BindView(R.id.rv_list)
+    RecyclerView rvList;
+    Unbinder unbinder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,24 @@ public class MycommunityFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pub_fragment_mycom_layout, null);
+        View view = inflater.inflate(R.layout.pub_fragment_mycom_layout, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setListView();
+    }
+
+    private void setListView() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
